7f69b217be48e55759a33f7910fe2482ad7cdff6
saulenavarro <saulnavarrotec@gmail.com>
2024-02-15 01:40:32 -0600

    Initial commit

---
+++ test/palacetest.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Palacetest from '../lib/palacetest-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/palacetest-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Palacetest.PalacetestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
